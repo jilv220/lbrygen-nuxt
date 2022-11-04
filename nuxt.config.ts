f77649ed39fa7de06c1079d6801643829f4a8139
jilv220 <LyuJi220@hotmail.com>
2022-11-04 16:35:49 -0400

    imporve seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,10 +18,63 @@ export default defineNuxtConfig({
       viewport:
         "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
       meta: [
+        // title
+        {
+          property: "og:title",
+          content: "Lbrygen",
+        },
+        {
+          name: "twitter:title",
+          content: "Lbrygen",
+        },
+        // url
+        {
+          property: "og:url",
+          content: "https://lbrygen.xyz",
+        },
+        {
+          name: "twitter:url",
+          content: "https://lbrygen.xyz",
+        },
+        // description
         {
           name: "description",
           content:
             "Alternative frontend for LBRY and Odysee written in Nuxt3. It is up just in case that Odysee is down!",
+        },
+        {
+          property: "og:description",
+          content:
+            "Alternative frontend for LBRY and Odysee written in Nuxt3. It is up just in case that Odysee is down!",
+        },
+        {
+          name: "twitter:description",
+          content:
+            "Alternative frontend for LBRY and Odysee written in Nuxt3. It is up just in case that Odysee is down!",
+        },
+        // OG ext
+        {
+          property: "og:sitename",
+          content: "Lbrygen",
+        },
+        {
+          property: "og:type",
+          content: "website",
+        },
+        {
+          property: "og:sitename",
+          content: "Lbrygen",
+        },
+        // Twitter ext
+        {
+          property: "twitter:card",
+          content: "summary_large_image",
+        },
+      ],
+      link: [
+        {
+          rel: "canonical",
+          href: "https://lbrygen.xyz",
         },
       ],
     },
